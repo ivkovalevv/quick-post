06fd8fbf6aad63247c51ebb1f8ed325675f0d012
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-06-30 14:53:47 +0000

    dark & light theme styles

--- src/components/UI/navbar/Navbar.jsx
+++ src/components/UI/navbar/Navbar.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = (props) =>{
-    const themeHandler = () => {
-      props.theme ? props.themeHandler(false) : props.themeHandler(true);
+    const themeHandler = (e) => {
+      e.stopPropagation();
+      return props.theme ? props.themeHandler(false) : props.themeHandler(true);
     }
 
     return(
@@ -11,7 +12,42 @@ const Navbar = (props) =>{
         <div className="navbar__links">
           <Link className="navbar__link" to="/about">About</Link>
           <Link className="navbar__link" to="/posts">Posts</Link>
-          <button className="color-theme-button" onClick={() => themeHandler()}>C</button>
+          <button className="color-theme-button" onClick={(e) => themeHandler(e)}>
+            <label
+              htmlFor="themeToggle"
+              className="themeToggle st-sunMoonThemeToggleBtn"
+              type="checkbox"
+            >
+              <input type="checkbox" id="themeToggle" className="themeToggleInput" />
+              <svg
+                width="18"
+                height="18"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                stroke="none"
+              >
+                <mask id="moon-mask">
+                  <rect x="0" y="0" width="20" height="20" fill="white"></rect>
+                  <circle cx="11" cy="3" r="8" fill="black"></circle>
+                </mask>
+                <circle
+                  className="sunMoon"
+                  cx="10"
+                  cy="10"
+                  r="8"
+                  mask="url(#moon-mask)"
+                ></circle>
+                <g>
+                  <circle className="sunRay sunRay1" cx="18" cy="10" r="1.5"></circle>
+                  <circle className="sunRay sunRay2" cx="14" cy="16.928" r="1.5"></circle>
+                  <circle className="sunRay sunRay3" cx="6" cy="16.928" r="1.5"></circle>
+                  <circle className="sunRay sunRay4" cx="2" cy="10" r="1.5"></circle>
+                  <circle className="sunRay sunRay5" cx="6" cy="3.1718" r="1.5"></circle>
+                  <circle className="sunRay sunRay6" cx="14" cy="3.1718" r="1.5"></circle>
+                </g>
+              </svg>
+            </label>
+          </button>
         </div>
       </div>
     )
