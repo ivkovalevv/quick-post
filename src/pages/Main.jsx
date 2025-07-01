999e0e90499af6a82a9213f379b3478638cd07de
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-07-01 19:30:01 +0300

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,8 +4,20 @@ const Main = () =>{
     return (
         <div className="page-wrapper">
             <h1 className="about-title">
-                This is SPA application for working with posts: loading from API, creating, sorting and searching. Written in React using class components.
+                Welcome to Quick Post!
             </h1>
+            <h3>
+                Quick Post is a sleek SPA for managing posts. Here’s what you can do:
+            </h3>
+            <ul>
+                <li>Fetch posts from DummyJSON API</li>
+                <li>Search by title or content</li>
+                <li>Sort by name or description</li>
+                <li>Add or delete posts</li>
+                <li>Explore posts in detail</li>
+            </ul>
+            <p>Click the button below to get started!</p>
+            <button>Get started</button>
         </div>
     )
 }
