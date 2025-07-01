acd6a815dba646611475e27ac588da9bfe475740
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-07-01 19:18:30 +0300

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,7 +4,7 @@ const Main = () =>{
     return (
         <div className="page-wrapper">
             <h1 className="about-title">
-                This is main page.
+                This is SPA application for working with posts: loading from API, creating, sorting and searching. Written in React using class components.
             </h1>
         </div>
     )
