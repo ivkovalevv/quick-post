68f48d55880c5bd89576999c559eeb1342f40165
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-07-01 19:21:49 +0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,8 @@ const About = () =>{
         <div className="page-wrapper">
             <h1 className="about-title">
                 This application was created in 2023 as part of the study of the React library and its class components.
+                <br></br>
+                It's SPA application for working with posts: loading from API, creating, sorting and searching.
             </h1>
         </div>
     )
