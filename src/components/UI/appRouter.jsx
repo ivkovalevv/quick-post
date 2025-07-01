6f3bc4f714d1a6b817933a9a056dbe13efd73373
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-07-01 14:50:39 +0000

    add main page

--- src/components/UI/appRouter.jsx
+++ src/components/UI/appRouter.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Main from "../../pages/Main";
 import About from "../../pages/About";
 import Posts from "../../pages/Posts"
 import PostsIdPage from "../../pages/PostIdPage"
@@ -7,6 +8,7 @@ import { Route, Routes} from "react-router-dom";
 const AppRouter = () =>{
     return(
         <Routes>
+          <Route path="/" element={<Main></Main>}></Route>
           <Route path="/about" element={<About></About>}></Route>
           <Route path="/posts" element={<Posts></Posts>}></Route>
           <Route exact path="/posts/:id" element={<PostsIdPage></PostsIdPage>}></Route>
---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () =>{
+    return (
+        <div className="page-wrapper">
+            <h1 className="about-title">
+                This is main page.
+            </h1>
+        </div>
+    )
+}
+
+export default Main
