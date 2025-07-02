bbca3a0cc6759691ef739760efe8acf1b6e18b6c
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-07-02 07:44:49 +0000

    add new description in about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,9 +6,9 @@ const About = () =>{
             <h1 className="about-title">
                 This application was created in 2023 as part of the study of the React library and its class components.
                 <br></br>
-                
             </h1>
             <h3 className="about-description">It's SPA application for working with posts: loading from API, creating, sorting and searching.</h3>
+            <h3 className="about-description" style={{marginTop: "25px"}}>Redesigned in 2025.</h3>
         </div>
     )
 }
