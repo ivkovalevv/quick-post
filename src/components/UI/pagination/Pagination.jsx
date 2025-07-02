a5a9087cf093c61ef97f1e7fb0c5d550449b52c2
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-07-02 06:53:31 +0000

    add pages

--- src/components/UI/pagination/Pagination.jsx
+++ src/components/UI/pagination/Pagination.jsx
@@ -5,14 +5,16 @@ const Pagination = ({totalPages, page, changePage}) =>{
     let pagesArray = getPagesArray(totalPages)
     return (
         <div className="page__wrapper">
-        {pagesArray.map(p =>
-          <span 
-          onClick={() => changePage(p)}
-          key={p} 
-          className={page === p? "page page__current" : "page"}>
-            {p}
-          </span>
-        )}
+          <div className="pages">
+            {pagesArray.map(p =>
+              <span 
+              onClick={() => changePage(p)}
+              key={p} 
+              className={page === p? "page page__current" : "page"}>
+                {p}
+              </span>
+            )}
+          </div>
       </div>
     )
 }
