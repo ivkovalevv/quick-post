8525a97fe364bc2f93e658255d4cd72084a0f685
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2025-07-02 07:24:49 +0000

    add go back button

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useFetching } from "../hooks/useFetching";
 import Loader from "../components/UI/loader/Loader";
 import PostService from "../API/PostService"
@@ -27,15 +27,16 @@ const PostIdPage = () => {
 
     return(
         <div className="page-wrapper">
+            <Link className="back__link" to="/posts">&#8592;</Link>
             <h1 className="small-description" style={{textAlign: "left"}}>You have opened the page of the post with ID {params.id}</h1>
             {isLoading
                 ? <Loader></Loader>
                 : <div>
                     <div className="post__title">{post.id}. {post.title}</div>
-                    {comments.comments.length ? <h2>Comments</h2> : null}
-                    <div>
+                    <div className="comments">
+                        {comments.comments.length ? <h2 className="post__title">Comments:</h2> : <h2 className="post__title">There are no comments for this post.</h2>}
                         {comments.comments.map(comment =>
-                            <div style={{marginTop: 10}}>
+                            <div className="post__description comment" style={{marginTop: 10}}>
                                 <div>{comment.body}</div>
                             </div>
                         )}
